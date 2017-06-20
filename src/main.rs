0139877a26d5b636dcaaf89ab5a8f23840f333dd
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2017-06-20 12:20:56 +0200

    enable loop and multi-file input

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,13 @@ fn main() {
   let host = matches.value_of("host").unwrap();
   let server_ip = matches.value_of("server-ip").unwrap();
   let server_port = matches.value_of("server-port").unwrap();
-  let source_file = matches.value_of("file").unwrap();
-  
+  let source_files: Vec<_> = matches.values_of("file").unwrap().collect();
+
   let loop_enabled = matches.is_present("loop");
   let bitrate = value_t!(matches.value_of("bitrate"), u64).expect("unable to parse MpegTS bitrate");
 
   println!("From    : {}:{}", server_ip, server_port);
   println!("To      : {}:{}", host, port);
-  println!("File    : {}", source_file);
   println!("Loop    : {}", loop_enabled);
   println!("Bitrate : {}", bitrate);
 
@@ -36,24 +35,43 @@ fn main() {
   
   let socket = UdpSocket::bind(bind_ip).expect("couldn't bind to address");
 
-  let mut file = File::open(source_file).expect("unable to open file");
   let _r = socket.set_broadcast(true);
   socket.connect(broadcast_ip).expect("connect function failed");
 
-  let nano_sec_sleep = 665778; // 1 packet at 100Mbps
   let mut total_data_sended : u64 = 0;
 
+  loop {
+    for filepath in source_files.clone() {
+      stream_file(filepath, &socket, bitrate, &mut total_data_sended);
+    }
+    if !loop_enabled {
+      println!("End of streaming.");
+      return
+    }
+  }
+}
+
+fn stream_file(source_file: &str, socket: &std::net::UdpSocket, bitrate: u64, mut total_data_sended: &mut u64) {
+  println!("Stream source file : {}", source_file);
+  let mut file = File::open(source_file).expect("unable to open file");
+
+  let nano_sec_sleep = 665778; // 1 packet at 100Mbps
   let start_time = Instant::now();
 
   loop {
     let elasped = start_time.elapsed();
     let diff = elasped.as_secs() * 1000000000 + elasped.subsec_nanos() as u64;
 
-    while diff / 1000 * bitrate > total_data_sended * 1000000 {
+    while diff / 1000 * bitrate > *total_data_sended * 1000000 {
       let mut buffer = [0; BUFFER_SIZE];
-      let _size = file.read_exact(&mut buffer);
+      match file.read_exact(&mut buffer) {
+        Ok(_) => {},
+        Err(_msg) => {
+          return;
+        }
+      }
       let _res = socket.send(&buffer);
-      total_data_sended += (BUFFER_SIZE * 8) as u64;
+      *total_data_sended += (BUFFER_SIZE * 8) as u64;
     }
 
     sleep(Duration::new(0, nano_sec_sleep as u32));
